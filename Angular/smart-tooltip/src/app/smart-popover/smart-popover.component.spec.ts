6c792b3af49f40efe5a6ee1cc51709d4e884d0ef
Stan Kislyuk <stan_kislyuk@trimble.com>
2019-06-27 11:55:52 -0400

    Added smart-tooltip project

---
+++ Angular/smart-tooltip/src/app/smart-popover/smart-popover.component.spec.ts
@@ -0,0 +1,26 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+/*
+import { SmartPopoverComponent } from './smart-popover.component';
+
+describe('SmartPopoverComponent', () => {
+  let component: SmartPopoverComponent;
+  let fixture: ComponentFixture<SmartPopoverComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SmartPopoverComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SmartPopoverComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+*/
---
+++ Angular/smart-tooltip/src/app/smart-popover/smart-popover.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+import {PopoverModule} from "ngx-smart-popover";
+ 
+@Component({
+    selector: "app-smart-popover",
+    templateUrl:'./smart-popover.component.html',
+    styleUrls: ['./smart-popover.component.css']
+})
+export class SmartPopoverComponent {
+ 
+}
