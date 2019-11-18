508485b0d0cd4786ec0a1323ddd745ddcfd4d480
Stan Kislyuk <stan_kislyuk@trimble.com>
2019-11-18 16:27:30 -0500

    StripHTML string function

--- Utils/StringUtils.cs
+++ Utils/StringUtils.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Utils
 {
@@ -14,5 +16,12 @@ namespace Utils
 				 .Where(c => !Char.IsWhiteSpace(c))
 				 .ToArray());
 		}
-	}
+
+        public static string StripHTML(this string HTMLText, bool decode = true)
+        {
+            Regex reg = new Regex("<[^>]+>", RegexOptions.IgnoreCase);
+            var stripped = reg.Replace(HTMLText, "");
+            return decode ? HttpUtility.HtmlDecode(stripped) : stripped;
+        }
+    }
 }
