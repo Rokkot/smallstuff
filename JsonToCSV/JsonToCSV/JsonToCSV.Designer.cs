91a072efb6972e8ff1bccc5e92712ca7061b7627
Stan Kislyuk <stan_kislyuk@trimble.com>
2021-02-09 17:17:17 -0500

    JSON to CSV converter

---
+++ JsonToCSV/JsonToCSV/JsonToCSV.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace JsonToCSV
+{
+    partial class JsonToCSV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_JSON = new System.Windows.Forms.Button();
+            this.button_CSV = new System.Windows.Forms.Button();
+            this.button_Convert = new System.Windows.Forms.Button();
+            this.textBox_Json = new System.Windows.Forms.TextBox();
+            this.textBox_CSV = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_JSON
+            // 
+            this.button_JSON.Location = new System.Drawing.Point(665, 12);
+            this.button_JSON.Name = "button_JSON";
+            this.button_JSON.Size = new System.Drawing.Size(75, 23);
+            this.button_JSON.TabIndex = 0;
+            this.button_JSON.Text = "...";
+            this.button_JSON.UseVisualStyleBackColor = true;
+            this.button_JSON.Click += new System.EventHandler(this.button_JSON_Click);
+            // 
+            // button_CSV
+            // 
+            this.button_CSV.Location = new System.Drawing.Point(665, 60);
+            this.button_CSV.Name = "button_CSV";
+            this.button_CSV.Size = new System.Drawing.Size(75, 23);
+            this.button_CSV.TabIndex = 1;
+            this.button_CSV.Text = "...";
+            this.button_CSV.UseVisualStyleBackColor = true;
+            this.button_CSV.Click += new System.EventHandler(this.button_CSV_Click);
+            // 
+            // button_Convert
+            // 
+            this.button_Convert.Location = new System.Drawing.Point(150, 101);
+            this.button_Convert.Name = "button_Convert";
+            this.button_Convert.Size = new System.Drawing.Size(509, 53);
+            this.button_Convert.TabIndex = 2;
+            this.button_Convert.Text = "Convert";
+            this.button_Convert.UseVisualStyleBackColor = true;
+            this.button_Convert.Click += new System.EventHandler(this.button_Convert_Click);
+            // 
+            // textBox_Json
+            // 
+            this.textBox_Json.Location = new System.Drawing.Point(150, 13);
+            this.textBox_Json.Name = "textBox_Json";
+            this.textBox_Json.Size = new System.Drawing.Size(509, 22);
+            this.textBox_Json.TabIndex = 3;
+            // 
+            // textBox_CSV
+            // 
+            this.textBox_CSV.Location = new System.Drawing.Point(150, 60);
+            this.textBox_CSV.Name = "textBox_CSV";
+            this.textBox_CSV.Size = new System.Drawing.Size(509, 22);
+            this.textBox_CSV.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Select JSON file";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Select output folder";
+            // 
+            // JsonToCSV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 171);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_CSV);
+            this.Controls.Add(this.textBox_Json);
+            this.Controls.Add(this.button_Convert);
+            this.Controls.Add(this.button_CSV);
+            this.Controls.Add(this.button_JSON);
+            this.Name = "JsonToCSV";
+            this.Text = "JsonToCSV";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_JSON;
+        private System.Windows.Forms.Button button_CSV;
+        private System.Windows.Forms.Button button_Convert;
+        private System.Windows.Forms.TextBox textBox_Json;
+        private System.Windows.Forms.TextBox textBox_CSV;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ JsonToCSV/JsonToCSV/JsonToCSV.cs
@@ -0,0 +1,202 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace JsonToCSV
+{
+    public partial class JsonToCSV : Form
+    {
+        public JsonToCSV()
+        {
+            InitializeComponent();
+        }
+
+        private void button_JSON_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (OpenFileDialog ofd = new OpenFileDialog())
+                {
+                    ofd.DefaultExt = ".json";
+                    ofd.Filter = "JSON (.json)|*.json";
+
+                    ofd.InitialDirectory = (string.IsNullOrEmpty(textBox_Json.Text) == false) ? textBox_Json.Text : "";
+
+                    if (ofd.ShowDialog(this) == DialogResult.OK)
+                    {
+                        textBox_Json.Text = ofd.FileName;
+                        textBox_CSV.Text = Path.GetDirectoryName(ofd.FileName);
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button_CSV_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (FolderBrowserDialog fd = new FolderBrowserDialog())
+                {
+                    fd.SelectedPath = (string.IsNullOrEmpty(textBox_CSV.Text) == false) ? textBox_CSV.Text : "";
+
+                    if (fd.ShowDialog(this) == DialogResult.OK)
+                    {
+                        textBox_CSV.Text = fd.SelectedPath;
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button_Convert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sCSV = string.Empty;
+                string sJSon = File.ReadAllText(textBox_Json.Text);
+
+                sCSV = ReadJson(sJSon);
+
+                if (string.IsNullOrEmpty(sCSV) == false)
+                {
+                    string sCSV_Path = Path.Combine(textBox_CSV.Text, Path.GetFileNameWithoutExtension(textBox_Json.Text));
+                    sCSV_Path += ".csv";
+
+                    File.WriteAllText(sCSV_Path, sCSV, Encoding.ASCII);
+
+                    if (File.Exists(sCSV_Path) == true)
+                    {
+                        System.Diagnostics.Process.Start(Path.GetDirectoryName(sCSV_Path));
+                    }
+                }
+                
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private string ReadJson(string jsonString)
+        {
+            JArray obj = JsonConvert.DeserializeObject<JArray>(jsonString);
+
+            if (obj == null)
+            {
+                throw new Exception("Faild to deserialize JSON file.");
+            }
+
+            StringBuilder result = new StringBuilder();
+            result.AppendLine(ReadHeadersFromJson(obj));
+            result.AppendLine(ReadDataFromJson(obj));
+
+            return result.ToString();
+        }
+
+        private string ReadDataFromJson(JArray obj, string sLine = "")
+        {
+            StringBuilder result = new StringBuilder();
+            StringBuilder line = new StringBuilder();
+            List<JArray> innerList = new List<JArray>();
+
+
+            string sMainLine = string.Empty;
+            string sTemp = string.Empty;
+            string sVaue = string.Empty;
+
+            if (obj == null)
+            {
+                throw new Exception("Faild to read JSON values");
+            }
+
+            foreach (JObject content in obj.Children<JObject>())
+            {
+                if (string.IsNullOrWhiteSpace(sLine) == false)
+                {
+                    line.Append(sLine);
+                }
+
+                foreach (JProperty prop in content.Properties())
+                {
+                    if (prop.Value is JArray)
+                    {
+                        innerList.Add((JArray)prop.Value);
+                    }
+                    else
+                    {
+                        sVaue = prop.Value.ToString();
+                        sVaue = sVaue.Replace(',', ' ');
+                        line.AppendFormat("{0}, ", sVaue);
+                    }
+                }
+
+                if (innerList.Count > 0)
+                {
+                    sMainLine = line.ToString();
+
+                    line.Clear();
+
+                    foreach (JArray val in innerList)
+                    {
+                        sTemp = ReadDataFromJson(val, sMainLine);
+                        line.Append(sTemp);
+                    }
+                }
+
+                innerList.Clear();
+
+                sTemp = line.ToString();
+                sTemp = sTemp.TrimEnd('\r', '\n', ' ');
+
+                result.AppendLine(sTemp);
+
+                line.Clear();
+            }
+
+            return result.ToString();
+        }
+
+        private string ReadHeadersFromJson(JArray obj)
+        {
+            StringBuilder result = new StringBuilder();
+
+            if (obj == null)
+            {
+                throw new Exception("Faild to read JSON headers");
+            }
+
+            if (obj != null && obj.First != null)
+            {
+                JObject content = (JObject)obj.First;
+
+                foreach (JProperty prop in content.Properties())
+                {
+                    if (prop.Value is JArray)
+                    {
+                        result.Append(ReadHeadersFromJson((JArray)prop.Value));
+                    }
+                    else
+                    {
+                        result.Append(string.Format("{0}, ", prop.Name));
+                    }
+                }
+
+            }
+
+            string sCSV = result.ToString();
+
+            return sCSV.TrimEnd(' ', ',');
+        }
+    }
+}
